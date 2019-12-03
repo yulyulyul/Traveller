339644c8277f6b34df2df29f198dfb09b2cb6eef
khyun <KHyun94>
2019-12-03 22:59:16 +0900

    ---진행중

---
+++ app/src/main/java/jso/kpl/traveller/network/CountryAPI.java
@@ -0,0 +1,33 @@
+package jso.kpl.traveller.network;
+
+import java.util.List;
+
+import jso.kpl.traveller.model.Country;
+import jso.kpl.traveller.model.ResponseResult;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface CountryAPI {
+
+    @FormUrlEncoded
+    @POST("/load_country_list")
+    Call<ResponseResult<List<Country>>> loadCountryList(@Field("u_userid") int u_userid);
+
+    @FormUrlEncoded
+    @POST("/load_favorite_country")
+    Call<ResponseResult<List<Country>>> loadFavoriteCountry(@Field("u_userid") int u_userid, @Field("is_mypage") int is_mypage);
+
+    @FormUrlEncoded
+    @POST("/add_country")
+    Call<ResponseResult<Integer>> addFlag(@Field("u_userid") int u_userid, @Field("ct_no") int ct_no);
+
+    @FormUrlEncoded
+    @POST("/delete_favorite_country")
+    Call<ResponseResult<Integer>> deleteFavoriteCountry(@Field("u_userid") int u_userid, @Field("ct_no") int ct_no);
+
+    @FormUrlEncoded
+    @POST("load_country_info")
+    Call<ResponseResult<List<Country>>> loadCountryInfo(@Field("u_userid") int u_userid, @Field("ct_no") int ct_no);
+}
